1ce385b37f33eda71671e5f7b1e988d01428fafb
Anna Evseenkova <annion25@gmail.com>
2024-03-29 10:13:59 -0400

    errors fixed

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # qa_python
+
+tests.py::TestBooksCollector::test_add_new_book_add_two_books_added_two PASSED [  7%]
+#тест проверяет, что добавилось 2 книги
+
+tests.py::TestBooksCollector::test_add_new_book_add_one_book_empty_genre PASSED [ 15%]
+#тест проверяет, что у добавленной книги пустой жанр
+
+tests.py::TestBooksCollector::test_set_book_genre_with_name_and_genre PASSED [ 23%]
+#тест проверяет, что установился жанр
+
+tests.py::TestBooksCollector::test_get_book_genre_correct_name_with_genre PASSED [ 30%]
+#тест проверяет, что метод отдает жанр книги по названию
+
+tests.py::TestBooksCollector::test_get_books_with_specific_genre_two_books_from_three PASSED [ 38%]
+#тест проверяет, что метод отдает список книг по жанрам
+
+tests.py::TestBooksCollector::test_get_books_genre_three_books PASSED     [ 46%]
+#тест проверяет словарь books_genre по размеру и составу
+
+tests.py::TestBooksCollector::test_get_books_for_children_two_books_from_four PASSED [ 53%]
+#тест проверяет возврат подходящих и неподходящих книг
+
+tests.py::TestBooksCollector::test_add_book_in_favorites_new_book PASSED [ 61%]
+#тест проверяет добавление 2х книг в избранное
+
+tests.py::TestBooksCollector::test_delete_book_from_favorites PASSED     [ 69%]
+#тест проверяет удаление книги из избранного
+
+tests.py::TestBooksCollector::test_get_list_of_favorites_books[\u0433\u0430\u0440\u0440\u0438] PASSED [ 76%]
+tests.py::TestBooksCollector::test_get_list_of_favorites_books[\u0433\u0430\u0440\u0440\u0438 \u043f\u043e\u0436\u0430\u0440] PASSED [ 84%]
+tests.py::TestBooksCollector::test_get_list_of_favorites_books[\u0433\u0430\u0440\u0440\u0438 \u0433\u043e\u0440\u0438\u0442] PASSED [ 92%]
+tests.py::TestBooksCollector::test_get_list_of_favorites_books[\u0433\u0430\u0440\u0440\u0438 \u0443\u0448\u0435\u043b] PASSED [100%]
+#тесты проверяют добавление книг в избранное с параметрами
--- tests.py
+++ tests.py
@@ -2,58 +2,53 @@ import pytest
 
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+    def test_add_new_book_add_two_books_added_two(self):
         collector = BooksCollector()
 
-        # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert len(collector.get_books_genre()) == 2
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_genre()) == 2 and collector.get_book_genre('Гордость и предубеждение и зомби') == ''
+    def test_add_new_book_add_one_book_empty_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        assert collector.get_book_genre('Гордость и предубеждение и зомби') == ''
 
-    # напиши свои тесты ниже
     def test_set_book_genre_with_name_and_genre(self):
         collector = BooksCollector()
-        collector.add_new_book('Что делать, если ва')
-        collector.set_book_genre('Что делать, если ва', 'Ужасы')
-        assert 'Что делать, если ва' in collector.books_genre and collector.books_genre.get('Что делать, если ва') == 'Ужасы'
+        film_name = 'Что делать, если ва'
+        collector.add_new_book(film_name)
+        collector.set_book_genre(film_name, collector.genre[1])
+        assert film_name in collector.books_genre and collector.books_genre.get(film_name) == collector.genre[1]
 
     def test_get_book_genre_correct_name_with_genre(self):
         collector = BooksCollector()
-        collector.add_new_book('Дратути')
-        collector.set_book_genre('Дратути', 'Комедии')
-        assert collector.get_book_genre('Дратути') == 'Комедии'
+        film_name = 'Дратути'
+        collector.add_new_book(film_name)
+        collector.set_book_genre(film_name, collector.genre[4])
+        assert collector.get_book_genre(film_name) == collector.genre[4]
 
-    def test_get_books_with_specific_genre_two_books_from_tree(self):
+    def test_get_books_with_specific_genre_two_books_from_three(self):
         collector = BooksCollector()
         collector.add_new_book('Что')
         collector.add_new_book('Что делать')
         collector.add_new_book('Что делать, если')
-        collector.set_book_genre('Что', 'Ужасы')
-        collector.set_book_genre('Что делать', 'Ужасы')
-        collector.set_book_genre('Что делать, если', 'Комедии')
-        assert len(collector.get_books_with_specific_genre('Ужасы')) == 2 and len(collector.get_books_with_specific_genre('Комедии')) == 1 and 'Что' in collector.get_books_with_specific_genre('Ужасы')
+        collector.set_book_genre('Что', collector.genre[1])
+        collector.set_book_genre('Что делать', collector.genre[1])
+        collector.set_book_genre('Что делать, если', collector.genre[4])
+        assert len(collector.get_books_with_specific_genre(collector.genre[1])) == 2 and len(collector.get_books_with_specific_genre(collector.genre[4])) == 1 and 'Что' in collector.get_books_with_specific_genre(collector.genre[1])
 
-    def test_get_books_genre_tree_books(self):
+    def test_get_books_genre_three_books(self):
         collector = BooksCollector()
         collector.add_new_book('Что')
         collector.add_new_book('Что делать')
         collector.add_new_book('Что делать, если')
-        collector.set_book_genre('Что', 'Ужасы')
-        collector.set_book_genre('Что делать', 'Ужасы')
-        collector.set_book_genre('Что делать, если', 'Комедии')
-        assert len(collector.books_genre) == 3 and collector.books_genre['Что'] == 'Ужасы' and collector.books_genre['Что делать'] == 'Ужасы' and collector.books_genre['Что делать, если'] == 'Комедии'
+        collector.set_book_genre('Что', collector.genre[1])
+        collector.set_book_genre('Что делать', collector.genre[1])
+        collector.set_book_genre('Что делать, если', collector.genre[4])
+        assert len(collector.books_genre) == 3 and collector.books_genre['Что'] == collector.genre[1] and collector.books_genre['Что делать'] == collector.genre[1] and collector.books_genre['Что делать, если'] == collector.genre[4]
 
     def test_get_books_for_children_two_books_from_four(self):
         collector = BooksCollector()
@@ -62,10 +57,10 @@ class TestBooksCollector:
         collector.add_new_book('поттер веселье')
         collector.add_new_book('поттер расследует')
 
-        collector.set_book_genre('гарри', 'Фантастика')
-        collector.set_book_genre('гарри кошмар', 'Ужасы')
-        collector.set_book_genre('поттер веселье', 'Комедии')
-        collector.set_book_genre('поттер расследует', 'Детектив')
+        collector.set_book_genre('гарри', collector.genre[0])
+        collector.set_book_genre('гарри кошмар', collector.genre[1])
+        collector.set_book_genre('поттер веселье', collector.genre[4])
+        collector.set_book_genre('поттер расследует', collector.genre[2])
 
         assert len(collector.get_books_for_children()) == 2 and 'гарри' in collector.get_books_for_children() and 'поттер веселье' in collector.get_books_for_children() and 'гарри кошмар' not in collector.get_books_for_children()
 
@@ -100,5 +95,3 @@ class TestBooksCollector:
         collector.add_new_book(name)
         collector.add_book_in_favorites(name)
         assert len(collector.favorites) == 1 and name in collector.favorites
-
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
